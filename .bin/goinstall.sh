28949717f61b45895ddcfcfbacc84daf2071f427
koki-algebra <biz.kokiyamada1022@gmail.com>
2024-06-26 22:30:31 +0900

    fix go install

--- .bin/goinstall.sh
+++ .bin/goinstall.sh
@@ -2,11 +2,9 @@
 
 packages=(
 	"golang.org/x/tools/gopls@latest"
+	"golang.org/x/tools/cmd/goimports@latest"
+	"go.uber.org/mock/mockgen@latest"
 	"github.com/99designs/gqlgen@latest"
-	"github.com/deepmap/oapi-codegen/cmd/oapi-codegen@latest"
-	"github.com/volatiletech/sqlboiler/v4@latest"
-	"github.com/volatiletech/sqlboiler/v4/drivers/sqlboiler-mysql@latest"
-	"github.com/volatiletech/sqlboiler/v4/drivers/sqlboiler-psql@latest"
 	"google.golang.org/protobuf/cmd/protoc-gen-go@latest"
 	"google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest"
 	"connectrpc.com/connect/cmd/protoc-gen-connect-go@latest"
